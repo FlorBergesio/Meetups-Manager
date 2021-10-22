a6c9cbc1eb3c31e9c85eaebd3be305eea0e3e72d
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2021-10-22 17:22:50 -0300

    Display a dummy list of meetups

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://images.pexels.com/photos/459301/pexels-photo-459301.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!'
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image: 'https://images.pexels.com/photos/459301/pexels-photo-459301.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup!'
+  }
+];
+
 const HomePage = () => {
   return (
-    <>
-    </>
+    <MeetupList meetups={ DUMMY_MEETUPS } />
   );
 };
  
