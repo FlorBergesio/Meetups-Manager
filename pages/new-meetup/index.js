b6960bcff9378ccda7529ee48a61472b8639c646
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2021-10-22 17:23:04 -0300

    Display new meetup form on /new-meetup

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,15 @@
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+const NewMeetupPage = () => {
+  const addMeetupHandler = ( enteredMeetupData ) => {
+    console.log(enteredMeetupData);
+  };
+
+  return (
+    <NewMeetupForm 
+      onAddMeetup={ addMeetupHandler }
+    />
+  );
+};
+ 
+export default NewMeetupPage;
