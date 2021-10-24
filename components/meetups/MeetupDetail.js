7122679b7ac77c1d7aac4525e9fac10f99b71c7b
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2021-10-24 15:56:29 -0300

    Added basic structure for Meetup Detail page and component

---
+++ components/meetups/MeetupDetail.js
@@ -0,0 +1,17 @@
+import styles from './MeetupDetail.module.css';
+
+const MeetupDetail = ( props ) => {
+  return (
+    <section className={ styles.detail }>
+      <img 
+        src={ props.image } 
+        alt={ props.title }  
+      />
+      <h1>{ props.title }</h1>
+      <address>{ props.address }</address>
+      <p>{ props.description }</p>
+    </section>
+  );
+}
+
+export default MeetupDetail;
---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = ( props ) => {
+  return (
+    <MeetupDetail 
+      image="https://images.pexels.com/photos/459301/pexels-photo-459301.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+      title="Meet"
+      address="addr"
+      description="des"
+    />
+  );
+}
+
+export default MeetupDetails;
