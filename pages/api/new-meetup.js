f994d0a4b28c5563e8247219b9a7f2a5b72c321d
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2021-10-25 12:36:34 -0300

    Added API endpoint to handle meetup creation

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,28 @@
+// /api/new-meetup
+// POST requests
+
+import { MongoClient } from 'mongodb';
+
+const mongodb_connection_string = 'mongodb://user_meetups_course:user_meetups_course_password@cluster0-shard-00-00.j3c7p.mongodb.net:27017,cluster0-shard-00-01.j3c7p.mongodb.net:27017,cluster0-shard-00-02.j3c7p.mongodb.net:27017/meetups?ssl=true&replicaSet=atlas-3rhyxh-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+async function handler( req, res ) {
+  if ( req.method === 'POST' ) {
+    try {
+      const data = req.body;
+
+      const client = await MongoClient.connect( mongodb_connection_string );
+      const db = client.db();
+
+      const meetupsCollection = db.collection('meetups');
+      const result = await meetupsCollection.insertOne( data );
+
+      client.close();
+
+      res.status(201).json({ message: "Meetup inserted!" });
+    } catch ( error ) {
+      res.status(500).json({ message: "An error occurred!" });
+    }
+  }
+}
+
+export default handler;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,21 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = ( enteredMeetupData ) => {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  const addMeetupHandler = async ( enteredMeetupData ) => {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify( enteredMeetupData ),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+
+    router.push('/');
   };
 
   return (
